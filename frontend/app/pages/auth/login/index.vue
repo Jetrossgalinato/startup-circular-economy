fd9699dfccb68c8c6b7628957cfa409e9e170730
Jetross <jetrossneri07@gmail.com>
2026-08-16 22:56:07 +0800

    Refactor login page layout in index.vue to include a structured card component for user authentication, enhancing user experience with form fields and action buttons. Update Nuxt.js configuration to specify component path for better organization.

--- frontend/app/pages/auth/login/index.vue
+++ frontend/app/pages/auth/login/index.vue
@@ -1,8 +1,47 @@
-<script setup lang="ts">
-
-</script>
 <template>
-    <div class="flex min-h-screen items-center justify-center">
-        <div class="text-4xl font-bold">This is the login page</div>
-    </div>
+  <div class="flex min-h-screen items-center justify-center">
+    <Card class="w-full max-w-sm">
+      <CardHeader>
+        <CardTitle>Login to your account</CardTitle>
+        <CardDescription>
+          Enter your email and password to login
+        </CardDescription>
+        <CardAction>
+          <Button variant="link" as-child>
+            <NuxtLink to="/auth/register">Sign up</NuxtLink>
+          </Button>
+        </CardAction>
+      </CardHeader>
+      <CardContent>
+        <form>
+          <div class="grid w-full items-center gap-4">
+            <div class="flex flex-col space-y-1.5">
+              <Label for="email">Email</Label>
+              <Input id="email" type="email" placeholder="m@example.com" />
+            </div>
+            <div class="flex flex-col space-y-1.5">
+              <div class="flex items-center">
+                <Label for="password">Password</Label>
+                <a
+                  href="#"
+                  class="ml-auto inline-block text-sm underline"
+                >
+                  Forgot your password?
+                </a>
+              </div>
+              <Input id="password" type="password" />
+            </div>
+          </div>
+        </form>
+      </CardContent>
+      <CardFooter class="flex flex-col gap-2">
+        <Button class="w-full">
+          Login
+        </Button>
+        <Button variant="outline" class="w-full">
+          Login with Google
+        </Button>
+      </CardFooter>
+    </Card>
+  </div>
 </template>
