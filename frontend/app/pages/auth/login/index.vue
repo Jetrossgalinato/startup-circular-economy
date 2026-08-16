e8b53b9d2dd52696ef61ca99185f2945d0e5b5d6
Jetross <jetrossneri07@gmail.com>
2026-08-16 22:37:48 +0800

    Add @vueuse/core dependency and update Tailwind CSS imports for improved styling and functionality.

---
+++ frontend/app/pages/auth/login/index.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+
+</script>
+<template>
+    <div class="flex min-h-screen items-center justify-center">
+        <div class="text-4xl font-bold">This is the login page</div>
+    </div>
+</template>
